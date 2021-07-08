d54b142a36ac4cad25159af7e0f5287f0c8dc69e
S. PREM RAJ <53055573+Premmmm@users.noreply.github.com>
2021-07-08 20:20:11 +0530

    Update project_database.py

--- project_database.py
+++ project_database.py
@@ -1,5 +1,5 @@
 import mysql.connector
-mydb=mysql.connector.connect(host="localhost",user="root",passwd="premraj123!",database="timesheet")
+mydb=mysql.connector.connect(host="localhost",user="root",passwd="",database="timesheet")
 
 mycursor=mydb.cursor()
 
